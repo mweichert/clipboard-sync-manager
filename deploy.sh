e95e13a27a0242879588c9c01e27d3382a3ca8cc
Michael Weichert <mweichert@gmail.com>
2024-03-28 23:03:08 -0400

    Version bump to 0.10

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
-# Read current version
-version=$(sed -nE 's/pkgver=([0-9]+\.[0-9]+)/\1/p' arch-packages/client/PKGBUILD)
+if [ -n "$1" ]; then
+    new_version="$1"
+else
+    # Read current version
+    version=$(sed -nE 's/pkgver=([0-9]+\.[0-9]+)/\1/p' arch-packages/client/PKGBUILD)
 
-# Check if version was captured
-if [ -z "$version" ]; then
-    echo "Version not found."
-    exit 1
+    # Check if version was captured
+    if [ -z "$version" ]; then
+        echo "Version not found."
+        exit 1
+    fi
+
+    echo "Current version is $version"
+
+    # Increment version
+    # Split the version number into major and minor parts
+    major=$(echo "$version" | cut -d. -f1)
+    minor=$(echo "$version" | cut -d. -f2)
+
+    # Increment the minor part
+    new_minor=$((minor + 1))
+
+    # Construct the new version
+    new_version="$major.$new_minor"
 fi
-
-echo "Current version is $version"
-
-# Increment version
-# Split the version number into major and minor parts
-major=$(echo "$version" | cut -d. -f1)
-minor=$(echo "$version" | cut -d. -f2)
-
-# Increment the minor part
-new_minor=$((minor + 1))
-
-# Construct the new version
-new_version="$major.$new_minor"
 
 echo "New version is $new_version"
 
+# Get the release number from the PKGBUILD
+release_number=$(sed -nE 's/pkgver=([0-9]+)/\1/p' arch-packages/client/PKGBUILD)
+next_release=$((release_number + 1))
+
 # Replace the version in PKGBUILD
-sed -i "s/pkgver=$version/pkgver=$new_version/" arch-packages/client/PKGBUILD
-                                                                                                                                 
+sed -i "s/pkgver=.*/pkgver=$new_version/" arch-packages/client/PKGBUILD
+sed -i "s/pkgver=.*/pkgver=$new_version/" arch-packages/server/PKGBUILD
+
+
+# Replace the release number in the PKGBUILD
+sed -i "s/pkgrel=.*/pkgrel=$next_release/" arch-packages/client/PKGBUILD
+sed -i "s/pkgrel=.*/pkgrel=$next_release/" arch-packages/server/PKGBUILD
                                                                                                                                                                                                                     
 # Git commit, tag and push                                                                                                                                                                                        
 git add arch-packages/client/PKGBUILD                                                                                                                                                                             
