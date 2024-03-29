9608df32f6217bb944a10c71c5df82b642dedc07
Michael Weichert <mweichert@gmail.com>
2024-03-28 22:25:22 -0400

    Version bump to 0.2

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# Read current version                                                                                                                                                                                            
-version=$(sed -nE 's/pkgver=([0-9]+)/\1/p' arch-packages/client/PKGBUILD)                                                                                                                                         
-                                                                                                                                                                                                                    
-# Increment version                                                                                                                                                                                               
-new_version=$((version + 1))                                                                                                                                                                                      
-                                                                                                                                                                                                                    
-# Replace the version in PKGBUILD                                                                                                                                                                                 
-sed -i "s/pkgver=$version/pkgver=$new_version/" arch-packages/client/PKGBUILD                                                                                                                                     
+# Read current version
+version=$(sed -nE 's/pkgver=([0-9]+\.[0-9]+)/\1/p' arch-packages/client/PKGBUILD)
+
+# Check if version was captured
+if [ -z "$version" ]; then
+    echo "Version not found."
+    exit 1
+fi
+
+echo "Current version is $version"
+
+# Increment version
+# Split the version number into major and minor parts
+major=$(echo "$version" | cut -d. -f1)
+minor=$(echo "$version" | cut -d. -f2)
+
+# Increment the minor part
+new_minor=$((minor + 1))
+
+# Construct the new version
+new_version="$major.$new_minor"
+
+echo "New version is $new_version"
+
+# Replace the version in PKGBUILD
+sed -i "s/pkgver=$version/pkgver=$new_version/" arch-packages/client/PKGBUILD
+                                                                                                                                 
                                                                                                                                                                                                                     
 # Git commit, tag and push                                                                                                                                                                                        
 git add arch-packages/client/PKGBUILD                                                                                                                                                                             
